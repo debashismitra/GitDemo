39d478991b766ca87ef4b6d9a975917a4e36fe00
debashis <deba.pop3@gmail.com>
2022-04-30 16:57:35 +0530

    direct commit to master

---
+++ src/main/java/Cucumber/CucumberFramework/App - Copy.java
@@ -0,0 +1,18 @@
+package Cucumber.CucumberFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		System.out.println( "Hello clone!" );
+		System.out.println( "Hello clone1!" );
+		System.out.println( "Hello main!" );
+		System.out.println( "Hello main!" );
+		System.out.println( "Hello modified branch!" );
+    }
+}
